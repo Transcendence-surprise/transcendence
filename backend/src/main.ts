e72a17dbddaa12d04fe95e9a4549e4ab300b8ac2
Misha <darkrabbit240@gmail.com>
2026-01-13 19:28:57 +0200

    refac: move dotenv from data-source to main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication, } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-if (process.env.APP_ENV !== 'production') {
-  require('dotenv').config();
-}
+dotenv.config(); // reads backend/.env when you run from backend/
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
