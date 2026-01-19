7371c7a6cfa3992ecfcb55a5ae4cdc2beaa997cb
Misha <darkrabbit240@gmail.com>
2026-01-19 04:06:29 +0200

    refac(docker): create base yml file, dev and prod

--- database/data-source.ts
+++ database/data-source.ts
@@ -6,11 +6,11 @@ import { User } from '../backend/src/users/user.entity';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST ?? 'localhost',
-  port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USER ?? 'transcendence',
-  password: process.env.DB_PASSWORD ?? 'transcendence',
-  database: process.env.DB_NAME ?? 'transcendence',
+  host: process.env.POSTGRES_HOST ?? 'localhost',
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  username: process.env.POSTGRES_USER ?? 'transcendence',
+  password: process.env.POSTGRES_PASSWORD ?? 'transcendence',
+  database: process.env.POSTGRES_DB ?? 'transcendence',
   entities: [User],
   migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
 });
