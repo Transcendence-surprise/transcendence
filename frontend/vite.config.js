ebae642a386e83c3e0b2770f93d8f54059367e20
Misha <darkrabbit240@gmail.com>
2026-01-24 15:49:38 +0200

    fix: update app for frontend-dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://backend:3000', // backend for docker network, localhost for local
         changeOrigin: true,
       },
       '/game': {                // <-- add this
-        target: 'http://localhost:3000',
+        target: 'http://backend:3000', // backend for docker network, localhost for local
         changeOrigin: true,
         secure: false,
       },
