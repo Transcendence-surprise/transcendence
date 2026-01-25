1074a657024627b8e8764c9cc6e62c70b1d9b2c8
Misha <darkrabbit240@gmail.com>
2026-01-26 01:51:12 +0200

    feat(frontend): add possible solution to proxy issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    // DELETE it if everything works
+    // Allow requests proxied from docker Compose service names
+    // allowedHosts: ['frontend', 'localhost', '127.0.0.1'],
     proxy: {
       '/api': {
         target: 'http://backend:3000', // backend for docker network, localhost for local
