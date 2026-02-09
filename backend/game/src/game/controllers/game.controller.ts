ea32cc3615eaf97f7d64a289e3ef3037ee00f3fd
Mariia Zhytnikova <allusio@gmail.com>
2026-02-09 17:31:42 +0200

    fix: optimize game DTO

--- backend/game/src/game/controllers/game.controller.ts
+++ backend/game/src/game/controllers/game.controller.ts
@@ -1,56 +1,48 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { EngineService } from '../services/engine.service.nest';
-import { GameState, GameSettings } from '../models/state';
 import { WsGateway } from '../../ws/ws.gateway';
-// import { BoardAction } from '../models/boardAction';
-// import { MoveAction } from '../models/moveAction';
-import { ApiBody, ApiOkResponse, ApiParam } from '@nestjs/swagger';
+import { ApiBody, ApiOkResponse, ApiParam, ApiResponse } from '@nestjs/swagger';
 import {
   CreateGameDto,
+  CreateGameResponseDto,
   StartGameDto,
+  StartResponseDto,
   JoinGameDto,
+  JoinResponseDto,
 //   MoveDto,
   LeaveGameDto,
+  LeaveResponseDto,
   GameStateDto,
+  SingleLevelDto,
+  MultiGameDto,
+  CheckPlayerAvailabilityDto,
 } from '../dtos/game.dto';
-import { SingleLevelDto } from '../dtos/level-registry.dto';
-import { MultiGameDto } from '../dtos/game-lobby-list.dto';
-import { MultiGame } from '../models/gameInfo';
-import { CheckPlayerAvailabilityDto } from '../dtos/checkPlayer.dto';
 
 
 @Controller('game')
 export class GameController {
   constructor(
-    private readonly engine: EngineService,
+  private readonly engine: EngineService,
   private readonly wsGateway: WsGateway,
 ) {}
 
   // Create Game
   @Post('create')
   @ApiBody({ type: CreateGameDto })
-  createGame(
-    @Body() body: {
-      hostId: string;
-      settings: GameSettings;
-    }
-  ) {
-    const { gameId } = this.engine.createGame(
-      body.hostId,
-      body.settings
-    );
+  @ApiResponse({ status: 201, type: CreateGameResponseDto })
+  createGame(@Body() body: CreateGameDto) : CreateGameResponseDto {
+    const { gameId } = this.engine.createGame(body.hostId, body.settings);
 
     this.wsGateway.sendMultiplayerListUpdate();
-    console.log("Game was created", gameId );
+    // console.log("Game was created", gameId );
     return { ok: true, gameId };
   }
 
   // Start game
   @Post('start')
   @ApiBody({ type: StartGameDto })
-  startGame(
-    @Body() body: { gameId: string; hostId: string }
-  ) {
+  @ApiResponse({ status: 201, type: StartResponseDto })
+  startGame(@Body() body: StartGameDto) : StartResponseDto {
     const result = this.engine.startGame(body.gameId, body.hostId);
 
     if (result.ok) {
@@ -63,16 +55,61 @@ export class GameController {
   // Join game
   @Post('join')
   @ApiBody({ type: JoinGameDto })
-  join(@Body() body: { gameId: string; playerId: string; role: "PLAYER" | "SPECTATOR" }) {
+  @ApiResponse({ status: 201, type: JoinResponseDto })
+  join(@Body() body: JoinGameDto) : JoinResponseDto {
     const result = this.engine.joinGame(body.gameId, body.playerId, body.role);
 
     if (result.ok) {
       this.wsGateway.sendMultiplayerListUpdate();
     }
-
-    console.log("User Joined: ", body.playerId );
-
+    // console.log("User Joined: ", body.playerId );
     return result;
+  }
+
+  @Post('leave')
+  @ApiBody({ type: LeaveGameDto })
+  @ApiResponse({ status: 201, type: LeaveResponseDto })
+  leaveGame(@Body() body: LeaveGameDto) : LeaveResponseDto {
+    const result = this.engine.leaveGame(body.gameId, body.playerId);
+
+    if (result.ok) {
+      this.wsGateway.sendMultiplayerListUpdate();
+    }
+
+    return result.ok ? { ok: true } : { ok: false, error: result.error };
+  }
+
+  @Get(':gameId')                                   // No need to return all state, only used dataXD
+  @ApiParam({ name: 'gameId', type: 'string' })
+  @ApiOkResponse({ type: GameStateDto })
+  getGameState(@Param('gameId') gameId: string) {
+    return this.engine.getGameState(gameId);
+  }
+
+  @Get('single/levels')
+  @ApiOkResponse({ type: SingleLevelDto, isArray: true })
+  getSingleLevels(): SingleLevelDto[] {
+    return this.engine.getSinglePlayerLevels();
+  }
+
+  @Get("multi/games")
+  @ApiOkResponse({ type: MultiGameDto, isArray: true })
+  getMultiplayerGames(): MultiGameDto[] {
+    return this.engine.getMultiGames();
+  }
+
+  @Get("check-player/:playerId")
+  @ApiParam({
+    name: "playerId",
+    type: String,
+    description: "Player ID to check availability across multiplayer games",
+  })
+  @ApiOkResponse({
+    description: "Player availability result",
+    type: CheckPlayerAvailabilityDto,
+  })
+  checkPlayer(@Param("playerId") playerId: string) {
+    return this.engine.checkPlayerAvailability(playerId);
   }
 
 //   // Make move
@@ -98,52 +135,5 @@ export class GameController {
 
 //     return result.ok ? { ok: true } : { ok: false, error: result.error };
 //   }
-
-  @Post('leave')
-  @ApiBody({ type: LeaveGameDto })
-  leaveGame(
-    @Body() body: { gameId: string; playerId: string }
-  ) {
-    const result = this.engine.leaveGame(body.gameId, body.playerId);
-
-    if (result.ok) {
-      this.wsGateway.sendMultiplayerListUpdate();
-    }
-
-    return result.ok ? { ok: true } : { ok: false, error: result.error };
-  }
-
-  @Get(':gameId')
-  @ApiParam({ name: 'gameId', type: 'string' })
-  @ApiOkResponse({ type: GameStateDto })
-  getGameState(@Param('gameId') gameId: string) {
-    return this.engine.getGameState(gameId);
-  }
-
-  @Get('single/levels')
-  @ApiOkResponse({ type: SingleLevelDto, isArray: true })
-  getSingleLevels(): SingleLevelDto[] {
-    return this.engine.getSinglePlayerLevels();
-  }
-
-  @Get("multi/games")
-  @ApiOkResponse({ type: MultiGameDto, isArray: true })
-  getMultiplayerGames(): MultiGame[] {
-    return this.engine.getMultiGames();
-  }
-
-  @Get("check-player/:playerId")
-  @ApiParam({
-    name: "playerId",
-    type: String,
-    description: "Player ID to check availability across multiplayer games",
-  })
-  @ApiOkResponse({
-    description: "Player availability result",
-    type: CheckPlayerAvailabilityDto,
-  })
-  checkPlayer(@Param("playerId") playerId: string) {
-    return this.engine.checkPlayerAvailability(playerId);
-  }
-
 }
+
