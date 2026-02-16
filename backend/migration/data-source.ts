9f0554e6584ea2b36490bb818da07562baec2093
Misha <darkrabbit240@gmail.com>
2026-02-16 23:56:05 +0200

    feat(db): check env to prevent fallback

--- backend/migration/data-source.ts
+++ backend/migration/data-source.ts
@@ -4,13 +4,23 @@ import { DataSource } from 'typeorm';
 
 import { ApiKey, Game, GamePlayer, User } from '@transcendence/db-entities';
 
+if (!(process.env.POSTGRES_HOST &&
+    Number(process.env.POSTGRES_PORT) &&
+    process.env.POSTGRES_USER &&
+    process.env.POSTGRES_PASSWORD &&
+    process.env.POSTGRES_DB))
+{
+      console.log('Error: some env variables not set');
+      process.exit(1);
+}
+
 export default new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST ?? 'localhost',
-  port: Number(process.env.POSTGRES_PORT ?? 5432),
-  username: process.env.POSTGRES_USER ?? 'transcendence',
-  password: process.env.POSTGRES_PASSWORD ?? 'transcendence',
-  database: process.env.POSTGRES_DB ?? 'transcendence',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [ApiKey, User, Game, GamePlayer],
   migrations: ['migrations/*.ts'],
   migrationsTableName: 'migrations',
