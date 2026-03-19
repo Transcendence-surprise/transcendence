ccddc5903bd48efa79d97465775eaf240b10b98f
marinezh <mazhivotova@gmail.com>
2026-03-19 23:57:20 +0200

    add global CSS variables and integrate with Tailwind config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { AuthProvider } from "./hooks/useAuth";
-import { installGlobalApiErrorHandling } from "./api/installGlobalApiErrorHandling";
-import "./index.css";
-import App from "./App";
-
-installGlobalApiErrorHandling();
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './hooks/useAuth'
+import "./global.css";
+import './index.css'
+import App from './App'
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
