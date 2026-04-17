b63a5befc437f84abcc16be4e75def887845ff26
Mariia Zhytnikova <allusio@gmail.com>
2026-04-17 15:22:01 +0300

    feat: save player in game to DB

---
+++ backend/game/src/game/engine/helpers/savePlayersTo.database.ts
@@ -0,0 +1,14 @@
+// src/game/engine/helpers/savePlayersTo.database.ts
+
+import { GameState } from "../../models/state";
+import { PlayersPersistenceService } from "../../services/playersPersistence.service";
+
+export async function savePlayersToDB(
+  gameId: string,
+  state: GameState,
+  persistence: PlayersPersistenceService,
+) {
+  console.log(`Saving players for game ${gameId} to database...`);
+  await persistence.savePlayer(gameId, state);
+  console.log(`Players for game ${gameId} saved to database.`);
+}
