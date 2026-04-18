a38dbb77aeecf8d58d2d7b758526c4b5b455508a
Misha <darkrabbit240@gmail.com>
2026-04-18 16:39:42 +0300

    refac: remove unused ts-client

--- common/ts-client/src/models/StartGameDto.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type StartGameDto = {
-    gameId: string;
-    hostId: string;
-};
-
